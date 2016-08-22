18fc41dabd2fae3d3047e497c0caeafd5db93453
TheKewlStore <thekewlstore@gmail.com>
2016-08-22 16:24:57 -0400

    Start of implementation for PGEWidget and Layouts.

--- pygame_engine/core/display.py
+++ pygame_engine/core/display.py
@@ -1,31 +1,38 @@
 """ Define the Display class, which manages the main window of the engine.
+
+    Author: Ian Davis
 """
 import pygame
 
 from pygame_engine.core.engine import Engine
-from pygame_engine.lib.data import color
 
 
 class Display(object):
     """ Represent a main display window for the PyGameEngine.
     """
-    # TODO: Setup basic widgets such as buttons and labels.
-    def __init__(self, size, main_widget=None, background_color=color.BLACK):
-        self.main_widget = main_widget
-        self.background_color = background_color
+
+    def __init__(self, size, main_widget=None):
+        self._main_widget = main_widget
         self.surface = pygame.display.set_mode(size)
         self.size = size
         self._register_default_callbacks()
+
+    @property
+    def main_widget(self):
+        return self._main_widget
+
+    @main_widget.setter
+    def main_widget(self, new_widget):
+        self._main_widget = new_widget
+        self._main_widget.surface = self.surface.subsurface()
 
     def paint(self):
         """ Handle painting this display and all of its widgets during the event loop.
 
             :return: None
         """
-        self.surface.fill(self.background_color)
-
-        if self.main_widget:
-            self.main_widget.blit()
+        if self._main_widget:
+            self._main_widget.paint()
 
     def _register_default_callbacks(self):
         """ Setup the default event signals and slots for this display.
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,6 @@
 from pygame_engine.core.engine import Engine
 from pygame_engine.core.display import Display
+from pygame_engine.lib.gui.widget import PGEWidget
 from pygame_engine.lib.data.size import Size
 
 
@@ -8,7 +9,8 @@ def startup():
 
         Used to create a display and test any dynamic runtime functionality.
     """
-    display = Display(Size(640, 480))
+    main_widget = PGEWidget(Size(640, 480))
+    display = Display(Size(640, 480), main_widget=main_widget)
 
 
 if __name__ == '__main__':
