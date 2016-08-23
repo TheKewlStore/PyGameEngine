bc19afd618eaea0a82abbd18f3406fa22f0dc898
TheKewlStore <thekewlstore@gmail.com>
2016-08-22 22:38:07 -0400

    Fixed painting to include the pygame.display.flip() necessary to show all changes to the screen after a paint.
    Updated .gitignore

--- pygame_engine/core/display.py
+++ pygame_engine/core/display.py
@@ -5,17 +5,23 @@
 import pygame
 
 from pygame_engine.core.engine import Engine
+from pygame_engine.lib.gui.widget import PGERootWidget
 
 
 class Display(object):
     """ Represent a main display window for the PyGameEngine.
     """
-
     def __init__(self, size, main_widget=None):
         self._main_widget = main_widget
         self.surface = pygame.display.set_mode(size)
         self.size = size
         self._register_default_callbacks()
+
+        if self._main_widget:
+            if not isinstance(self._main_widget, PGERootWidget):
+                raise TypeError('Main widget must be an instance of PGERootWidget')
+
+            self._main_widget.surface = self.surface.subsurface(self._main_widget.rect)
 
     @property
     def main_widget(self):
@@ -23,8 +29,11 @@ class Display(object):
 
     @main_widget.setter
     def main_widget(self, new_widget):
+        if not isinstance(new_widget, PGERootWidget):
+            raise TypeError('Main widget must be an instance of PGERootWidget')
+
         self._main_widget = new_widget
-        self._main_widget.surface = self.surface.subsurface()
+        self._main_widget.surface = self.surface.subsurface(self._main_widget.rect)
 
     def paint(self):
         """ Handle painting this display and all of its widgets during the event loop.
@@ -33,6 +42,8 @@ class Display(object):
         """
         if self._main_widget:
             self._main_widget.paint()
+
+        pygame.display.flip()
 
     def _register_default_callbacks(self):
         """ Setup the default event signals and slots for this display.
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,7 +1,13 @@
 from pygame_engine.core.engine import Engine
 from pygame_engine.core.display import Display
-from pygame_engine.lib.gui.widget import PGEWidget
+from pygame_engine.lib.gui.widget import PGERootWidget
+from pygame_engine.lib.data import color
 from pygame_engine.lib.data.size import Size
+
+
+class TestWidget(PGERootWidget):
+    def paint(self):
+        self.surface.fill(color.RED)
 
 
 def startup():
@@ -9,7 +15,7 @@ def startup():
 
         Used to create a display and test any dynamic runtime functionality.
     """
-    main_widget = PGEWidget(Size(640, 480))
+    main_widget = TestWidget(Size(640, 480))
     display = Display(Size(640, 480), main_widget=main_widget)
 
 
